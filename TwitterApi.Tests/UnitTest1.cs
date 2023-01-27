834c934d7ce500d9db0983c4796dff6b0e1db942
Maher Hujairi <maher.hujairi@toomitech.com>
2023-01-27 23:32:41 +0300

    code cleanup

--- TwitterApi.Tests/UnitTest1.cs
+++ TwitterApi.Tests/UnitTest1.cs
@@ -1,13 +1,5 @@
-
-using System.Net;
 using System.Net.Http.Headers;
 
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 namespace TwitterApi.Tests
