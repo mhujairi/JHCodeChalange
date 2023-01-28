8473a3ee0c98814fc838d3e3edf82f0d3d3f86ab
Maher Hujairi <maher.hujairi@toomitech.com>
2023-01-28 18:27:31 +0300

    Updated web client to show Top 10 Hashtags

--- JHCodeChalange/ClientApp/src/AppRoutes.js
+++ JHCodeChalange/ClientApp/src/AppRoutes.js
@@ -1,20 +1,20 @@
 import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
+import { TopHashTags } from "./components/TopHashTags";
 import { Home } from "./components/Home";
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+    {
+        index: true,
+        element: <Home />
+    },
+    {
+        path: '/counter',
+        element: <Counter />
+    },
+    {
+        path: '/TopHashtags',
+        element: <TopHashTags />
+    }
 ];
 
 export default AppRoutes;
---
+++ JHCodeChalange/ClientApp/src/components/TopHashTags.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+export class TopHashTags extends Component {
+    static displayName = TopHashTags.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { forecasts: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateHashTagsData();
+    }
+
+    static renderHashTagsTable(hashtags) {
+        return (
+            <table className="table table-striped" aria-labelledby="tableLabel">
+                <thead>
+                    <tr>
+                        <th>Hashag</th>
+                        <th>Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {hashtags.map(hashtag =>
+                        <tr key={hashtag.tag}>
+                            <td>{hashtag.tag}</td>
+                            <td>{hashtag.count}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : TopHashTags.renderHashTagsTable(this.state.hashtags);
+
+        return (
+            <div>
+                <h1 id="tableLabel">Top 10 Hashtags</h1>
+                {contents}
+
+                <button onClick={async (e) => { await this.populateHashTagsData() } }>Refresh</button>
+            </div>
+        );
+    }
+
+    async populateHashTagsData() {
+        const response = await fetch('Tweets/TopHashTags/10');
+        const data = await response.json();
+        this.setState({ hashtags: data, loading: false });
+    }
+}
